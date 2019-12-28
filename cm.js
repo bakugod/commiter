8829b8aa41f536fab175d55bca20744c00e43642
bakugod <bakugod@yandex.ru>
2019-12-28 22:36:32 +0300

    Dec 28 2019 22:36:31

--- cm.js
+++ cm.js
@@ -1,81 +1,22 @@
-const fs = require("fs");
 const dayjs = require("dayjs");
 const { exec, spawn } = require("child_process");
 
-const хуй = 0;
+const dateNow = () => dayjs(new Date()).format("MMM D YYYY HH:mm:ss");
 
-function rand() {
-  return (Math.random() * 6 + 4) | 0;
-}
+for (let i = 0; i < 4; i++) {
+  setTimeout(
+    () => exec("git pull", () => {
+      const worker = exec(
+        `node index.js && git add . && git commit -m "${dateNow()}" && git push`
+      );
 
-function replaceTime2(text, index, replacement) {
-  return (
-    text.substr(0, index) +
-    replacement +
-    text.substr(index + replacement.length)
+      worker.stdout.on("data", data => {
+        console.log("stdout: " + data);
+      });
+      worker.stderr.on("data", data => {
+        console.log("stderr: " + data);
+      });
+    }),
+    1000
   );
 }
-
-const templateString = `\n//${Math.random() * Date.parse(new Date())}`;
-
-let i = 0;
-
-function init(times) {
-  fs.readFile("./index.js", "utf8", (error, data) => {
-    if (error) {
-      return console.log(error);
-    }
-    const replaceTime = (text, index) =>
-      text.replace(`(${text[index]})`, `(${i})`);
-
-    const exSourse = data;
-    console.log(times);
-    const indexOfTime = exSourse.indexOf("й") + 5;
-    replaceTime(exSourse, indexOfTime);
-    const newSourse = replaceTime(exSourse, indexOfTime);
-
-    //`${exSourse} \n//${Math.random() * Date.parse(new Date())}`;
-
-    fs.writeFile("./index.js", `${newSourse + templateString}`, error => {
-      if (error) {
-        return console.log(error);
-      }
-      console.log("The file was saved!");
-    });
-  });
-}
-
-init(i);
-
-// while (i < 4) {
-//   init(i);
-// }
-
-const dateNow = () => dayjs(new Date()).format("MMM D YYYY HH:mm:ss");
-for (let i = 0; i < 4; i++) {
-  exec("git pull", err => {
-    const worker = exec(
-      `node index.js && git add . && git commit -m "${dateNow()}" && git push`
-    );
-
-    worker.stdout.on("data", data => {
-      console.log("stdout: " + data);
-    });
-    worker.stderr.on("data", data => {
-      console.log("stderr: " + data);
-    });
-  });
-}
-
-// const dateNow = () => dayjs(new Date()).format("MMM D YYYY HH:mm:ss");
-
-// while (i < 4) {
-//   init(i);
-// }
-
-// exec(
-//   `git add . && git commit -m "${dateNow()}" && git push`,
-//   (error, stdout, stderr) => {
-//     console.error(stdout);
-//   }
-// );
--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 const fs = require("fs");
+const dayjs = require("dayjs");
 
-0 <<
-  (function(times) {
-    fs.readFile("./index.js", "utf8", (error, data) => {
-      if (error) {
-        return console.log(error);
-      }
+const dateNow = () => dayjs(new Date()).format("MMM D YYYY HH:mm:ss");
 
-      fs.writeFile(
-        "./index.js",
-        `${data} \n//${Math.random() * Date.parse(new Date())}`,
-        error => {
-          if (error) {
-            return console.log(error);
-          }
-          console.log("The file was saved!");
+0<<function() {
+  fs.readFile("./index.js", "utf8", (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    fs.writeFile(
+      "./index.js",
+      `${data} \n//${dateNow()}`,
+      error => {
+        if (error) {
+          return console.log(error);
         }
-      );
-    });
-  })();
+        console.log("The file was saved!");
+      }
+    );
+  });
+}();
 
-//655590605671.6072 
-//1057870336391.8724 
+//655590605671.6072
+//1057870336391.8724
 //721577845944.9701
+//1252268028650.248
+ 
+//Dec 28 2019 22:36:31 
+//Dec 28 2019 22:36:31 
+//Dec 28 2019 22:36:31 
+//Dec 28 2019 22:36:31
