74f8bcf427d2ac9c3553d601d55a7744219aeb01
bakugod <bakugod@yandex.ru>
2019-12-28 22:33:02 +0300

    Dec 28 2019 22:33:02

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+const fs = require("fs");
 
-//1528680602428.909
+0 <<
+  (function(times) {
+    fs.readFile("./index.js", "utf8", (error, data) => {
+      if (error) {
+        return console.log(error);
+      }
+
+      fs.writeFile(
+        "./index.js",
+        `${data} \n//${Math.random() * Date.parse(new Date())}`,
+        error => {
+          if (error) {
+            return console.log(error);
+          }
+          console.log("The file was saved!");
+        }
+      );
+    });
+  })();
+
+//655590605671.6072 
+//1057870336391.8724 
+//721577845944.9701
