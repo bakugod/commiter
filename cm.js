1e8de9445152dec4f3adf28ef065308a1e53c6d2
bakugod <bakugod@yandex.ru>
2019-12-28 22:46:34 +0300

    Dec 28 2019 22:46:34

--- cm.js
+++ cm.js
@@ -5,19 +5,19 @@ const dateNow = () => dayjs(new Date()).format("MMM D YYYY HH:mm:ss");
 
 for (let i = 0; i < 4; i++) {
   setTimeout(
-    () => exec("git pull", () => {
-      console.log(i)
-      const worker = exec(
-        `node index.js && git add . && git commit -m "${dateNow()}" && git push`
-      );
+    () =>
+      exec("git pull", () => {
+        const worker = exec(
+          `node index.js && git add . && git commit -m "${dateNow()}" && git push`
+        );
 
-      // worker.stdout.on("data", data => {
-      //   console.log("stdout: " + data);
-      // });
-      // worker.stderr.on("data", data => {
-      //   console.log("stderr: " + data);
-      // });
-    }),
+        worker.stdout.on("data", data => {
+          console.log("stdout: " + data);
+        });
+        worker.stderr.on("data", data => {
+          console.log("stderr: " + data);
+        });
+      }),
     i * 1000
   );
 }
