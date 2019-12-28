c52b5ad6fc97cfdb1c4d796b1f3a6b704043e8ed
bakugod <bakugod@yandex.ru>
2019-12-28 22:54:16 +0300

    Dec 28 2019 22:54:16

--- cm.js
+++ cm.js
@@ -2,9 +2,9 @@ const dayjs = require("dayjs");
 const { exec, spawn } = require("child_process");
 
 const dateNow = () => dayjs(new Date()).format("MMM D YYYY HH:mm:ss");
-const rand = () => (Math.random() * 6 + 4) | 0;
+const rand = (() => (Math.random() * 6 + 4) | 0)();
 
-for (let i = 0; i < rand(); i++) {
+for (let i = 0; i < rand; i++) {
   setTimeout(
     () =>
       exec("git pull", () => {
