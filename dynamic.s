acecca9e12b8f7e97d563aabe6d848bcc2ac4bc9
bakugod <bakugod@yandex.ru>
2020-02-22 16:31:14 +0300

    Feb 22 2020 16:31:14

--- dynamic.s
+++ dynamic.s
@@ -4,15 +4,19 @@ global _start        ;must be declared for using gcc
     
 _start:	           ;tell linker entry point
 
-res	al,dx
-sub	edi,eax
-and	edx,si
-and	al,ah
-res	bx,eax
-res	esp,al
-res	edx,ebx
-res	ah,eax
-or	esi,eip
+and	bx,ah
+and	si,esi
+add	eax,ah
+int	al,sp
+aas	sp,di
+add	al,al
+and	cx,dx
+add	sp,ax
+mov	di,edx
+add	eip,ebp
+and	sp,eip
+aas	dx,ax
+res	edi,ebp
 
 
 mov	eax,1	        ;system call number (sys_exit)
